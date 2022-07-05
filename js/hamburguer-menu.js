9ed579dd001bf4d681c75c52309346e1ca4ed2f8
Desknik <106723609+Desknik@users.noreply.github.com>
2022-07-05 01:13:37 -0300

    header responsiva
    trabalhando a responsividade da pagina inicial, terminado a responsividade do header

---
+++ js/hamburguer-menu.js
@@ -0,0 +1,7 @@
+function sidebarOn() {
+    document.querySelector("header").classList.add(`active`);
+}
+
+function sidebarOff() {
+    document.querySelector("header").classList.remove(`active`);
+}
