af9aff2f43698db7b60c3ddc34df7d77d646d5a7
Desknik <106723609+Desknik@users.noreply.github.com>
2022-07-06 00:30:09 -0300

    concluido carrosel de imagens do site
    necessita de responsividade

---
+++ js/carousel.js
@@ -0,0 +1,59 @@
+var imagem = document.getElementById('atual');
+var voltar = document.getElementById('voltar');
+var proximo = document.getElementById('proximo');
+var quant = document.querySelectorAll('.content .imagens');
+var balls = document.querySelector('.balls');
+var atual = 0
+var imgAtual = document.getElementsByClassName("imgAtual")
+var rolar = true
+
+for (let i = 0; i < quant.length; i++) {
+  var div = document.createElement('div');
+  div.id = i
+  balls.appendChild(div)
+}
+document.getElementById('0').classList.add('imgAtual')
+
+var pos = document.querySelectorAll('.balls div')
+
+for (let i = 0; i < pos.length; i++) {
+    pos[i].addEventListener('click', ()=>{
+        atual = pos[i].id
+        console.log(atual)
+        rolar = false
+        slide()
+    })
+  }
+
+voltar.addEventListener('click', ()=>{
+    atual--
+    rolar = false
+    slide()
+})
+
+proximo.addEventListener('click', ()=>{
+    atual++
+    rolar = false
+    slide()
+})
+
+function slide(){
+    if (atual >= quant.length){
+    atual = 0
+    }else if(atual < 0){
+        atual = quant.length-1 
+    }
+   
+    document.querySelector('.imgAtual').classList.remove('imgAtual')
+    document.getElementById(atual).classList.add('imgAtual')
+    imagem.style.marginLeft = -1300*atual+'px'
+}
+
+setInterval(() => {
+   if (rolar){
+    atual++
+    slide()
+   }else{
+    rolar = true
+   }
+}, 4000);
